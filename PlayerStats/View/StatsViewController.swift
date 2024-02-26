f60bce0ddcc1551dd1469ee54711a63bcf75c215
jzb0228 <156571798+jzb0228@users.noreply.github.com>
2024-02-25 20:07:58 -0800

    Added back button

--- PlayerStats/View/StatsViewController.swift
+++ PlayerStats/View/StatsViewController.swift
@@ -58,15 +58,8 @@ class StatsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true)
     }
-    */
 
 }
